d85950f1ce2340c39438cc845dc36e48980fd8c7
Marek-lecian <marek.lecian@gmail.com>
2015-10-11 22:17:14 +0200

    Weather translate v1.00

---
+++ Yahoo-Weather-API-Czech-Translate.js
@@ -0,0 +1,186 @@
+  function preklad(weatherstring) {
+             if( weatherstring ==="AM Clouds / PM Sun") {return "mraky slunce";}
+        else if( weatherstring ==="AM Drizzle") {return "mrholení";}
+        else if( weatherstring ==="AM Drizzle / Wind") {return "mrholení vítr";}
+        else if( weatherstring ==="AM Fog / PM Clouds") {return "mlha mraky";}
+        else if( weatherstring ==="AM Fog / PM Sun") {return "mlha slunce";}
+        else if( weatherstring ==="AM Ice") {return "led";}
+        else if( weatherstring ==="AM Light Rain") {return "slabı déš";}
+        else if( weatherstring ==="AM Light Rain / Wind") {return "slabı déš vítr";}
+        else if( weatherstring ==="AM Light Snow") {return "slabé snìení";}
+        else if( weatherstring ==="AM Rain") {return "déš";}
+        else if( weatherstring ==="AM Rain / Snow Showers") {return "déš, snìhové pøeháòky";}
+        else if( weatherstring ==="AM Rain / Snow") {return "déš sníh";}
+        else if( weatherstring ==="AM Rain / Snow / Wind") {return "déš sníh vítr";}
+        else if( weatherstring ==="AM Rain / Wind") {return "déš vítr";}
+        else if( weatherstring ==="AM Showers") {return "pøeháòky";}
+        else if( weatherstring ==="AM Showers / Wind") {return "pøeháòky vítr";}
+        else if( weatherstring ==="AM Snow Showers") {return "snìhové pøeháòky";}
+        else if( weatherstring ==="AM Snow") {return "sníh";}
+        else if( weatherstring ==="AM Thundershowers") {return "polojasno";}
+        else if( weatherstring ==="Blowing Snow") {return "zvíøenı sníh";}
+        else if( weatherstring ==="Clear") {return "jasno";}
+        else if( weatherstring ==="Clear / Windy") {return "jasno vìtrno";}
+        else if( weatherstring ==="Clouds Early / Clearing Late") {return "mraky";}
+        else if( weatherstring ==="Cloudy") {return "zataeno";}
+        else if( weatherstring ==="Cloudy / Wind") {return "oblaèno vítr";}
+        else if( weatherstring ==="Cloudy / Windy") {return "oblaèno vìtrno";}
+        else if( weatherstring ==="Drifting Snow") {return "zvíøenı sníh";}
+        else if( weatherstring ==="Drifting Snow / Windy") {return "zvíøenı sníh vìtrno";}
+        else if( weatherstring ==="Drizzle Early") {return "mrholení";}
+        else if( weatherstring ==="Drizzle Late") {return "mrholení";}
+        else if( weatherstring ==="Drizzle") {return "mrholení";}
+        else if( weatherstring ==="Drizzle / Fog") {return "mrholení mlha";}
+        else if( weatherstring ==="Drizzle / Wind") {return "mrholení vítr";}
+        else if( weatherstring ==="Drizzle / Windy") {return "mrholení vìtrno";}
+        else if( weatherstring ==="Fair") {return "jasno";}
+        else if( weatherstring ==="Fair / Windy") {return "jasno vìtrno";}
+        else if( weatherstring ==="Few Showers") {return "dešové pøeháòky";}
+        else if( weatherstring ==="Few Showers / Wind") {return "dešové pøeháòky vítr";}
+        else if( weatherstring ==="Few Snow Showers") {return "nìkolik snìhové pøeháòky";}
+        else if( weatherstring ==="Fog Early / Clouds Late") {return "slabı vítr mraky";}
+        else if( weatherstring ==="Fog Late") {return "mlha";}
+        else if( weatherstring ==="Fog") {return "mlha";}
+        else if( weatherstring ==="Fog / Windy") {return "mlha vìtrno";}
+        else if( weatherstring ==="Foggy") {return "mlhavo";}
+        else if( weatherstring ==="Freezing Drizzle") {return "mrznoucí mrholení";}
+        else if( weatherstring ==="Freezing Drizzle / Windy") {return "mrznoucí mrholení vìtrno";}
+        else if( weatherstring ==="Freezing Rain") {return "mrznoucí déš";}
+        else if( weatherstring ==="Haze") {return "opar";}
+        else if( weatherstring ==="Heavy Drizzle") {return "tìkı mrholení";}
+        else if( weatherstring ==="Heavy Rain Shower") {return "silnı déš pøeháòky";}
+        else if( weatherstring ==="Heavy Rain") {return "hustı déš";}
+        else if( weatherstring ==="Heavy Rain / Wind") {return "silnı déš vítr";}
+        else if( weatherstring ==="Heavy Rain / Windy") {return "silnı déš vìtrno";}
+        else if( weatherstring ==="Heavy Snow Shower") {return "tìkı sníh pøeháòky";}
+        else if( weatherstring ==="Heavy Snow") {return "silné snìení";}
+        else if( weatherstring ==="Heavy Snow / Wind") {return "tìkı sníh vítr";}
+        else if( weatherstring ==="Heavy Thunderstorm") {return "tìkıch bouøka";}
+        else if( weatherstring ==="Heavy Thunderstorm / Windy") {return "tìkıch bouøka vìtrno";}
+        else if( weatherstring ==="Ice Crystals") {return "ledové krystaly";}
+        else if( weatherstring ==="Ice Late") {return "led";}
+        else if( weatherstring ==="Isolated T-storms") {return "izolované deštì";}
+        else if( weatherstring ==="Isolated Thunderstorms") {return "izolované bouøky";}
+        else if( weatherstring ==="Light Drizzle") {return "slabé mrholení";}
+        else if( weatherstring ==="Light Freezing Drizzle") {return "mrznoucí mrholení";}
+        else if( weatherstring ==="Light Freezing Rain") {return "mrznoucí déš";}
+        else if( weatherstring ==="Light Freezing Rain / Fog") {return "mrznoucí déš mlha";}
+        else if( weatherstring ==="Light Rain Early") {return "slabı déš";}
+        else if( weatherstring ==="Light Rain") {return "slabı déš";}
+        else if( weatherstring ==="Light Rain Late") {return "slabı déš";}
+        else if( weatherstring ==="Light Rain Shower") {return "slabı déš pøeháòky";}
+        else if( weatherstring ==="Light Rain Shower / Fog") {return "slabı déš pøeháòky mlha";}
+        else if( weatherstring ==="Light Rain Shower / Windy") {return "slabı déš pøeháòky vìtrno";}
+        else if( weatherstring ==="Light Rain with Thunder") {return "slabı déš se hromu";}
+        else if( weatherstring ==="Light Rain / Fog") {return "slabı déš mlha";}
+        else if( weatherstring ==="Light Rain / Freezing Rain") {return "slabı déš mrznoucí déš";}
+        else if( weatherstring ==="Light Rain / Wind Early") {return "slabı déš vítr";}
+        else if( weatherstring ==="Light Rain / Wind Late") {return "slabı déš vítr";}
+        else if( weatherstring ==="Light Rain / Wind") {return "slabı déš vítr";}
+        else if( weatherstring ==="Light Rain / Windy") {return "slabı déš vìtrno";}
+        else if( weatherstring ==="Light Sleet") {return "lehká plískanice";}
+        else if( weatherstring ==="Light Snow Early") {return "slabé snìení";}
+        else if( weatherstring ==="Light Snow Grains") {return "slabé snìení";}
+        else if( weatherstring ==="Light Snow Late") {return "slabé snìení";}
+        else if( weatherstring ==="Light Snow Shower") {return "slabé snìení pøeháòky";}
+        else if( weatherstring ==="Light Snow Shower / Fog") {return "slabé snìení pøeháòky mlha";}
+        else if( weatherstring ==="Light Snow with Thunder") {return "lehkı sníh s hromem";}
+        else if( weatherstring ==="Light Snow") {return "slabé snìení";}
+        else if( weatherstring ==="Light Snow / Fog") {return "slabı déš mlha";}
+        else if( weatherstring ==="Light Snow / Freezing Rain") {return "slabı déš mrznoucí déš";}
+        else if( weatherstring ==="Light Snow / Wind") {return "snìhové pøeháòky vítr";}
+        else if( weatherstring ==="Light Snow / Windy") {return "snìhové pøeháòky vìtrno";}
+        else if( weatherstring ==="Light Snow / Windy / Fog") {return "snìhové pøeháòky vìtrno mlha";}
+        else if( weatherstring ==="Mist") {return "mlha";}
+        else if( weatherstring ==="Mostly Clear") {return "vìtšinou jasnı";}
+        else if( weatherstring ==="Mostly Cloudy") {return "pøevánì zataeno";}
+        else if( weatherstring ==="Mostly Cloudy / Wind") {return "jasno vítr";}
+        else if( weatherstring ==="Mostly sunny") {return "polojasno";}
+        else if( weatherstring ==="Partial Fog") {return "èásteèná mlha";}
+        else if( weatherstring ==="Partly Cloudy") {return "polojasno";}
+        else if( weatherstring ==="Partly Cloudy / Wind") {return "èásteènì oblaèno vítr";}
+        else if( weatherstring ==="Patches of Fog") {return "chuchvalce mlhy";}
+        else if( weatherstring ==="Patches of Fog / Windy") {return "chuchvalce mlhy vìtrno";}
+        else if( weatherstring ==="PM Drizzle") {return "mrholení";}
+        else if( weatherstring ==="PM Fog") {return "mlha";}
+        else if( weatherstring ==="PM Light Snow") {return "slabé snìení";}
+        else if( weatherstring ==="PM Light Rain") {return "slabı déš";}
+        else if( weatherstring ==="PM Light Rain / Wind") {return "slabı déš vítr";}
+        else if( weatherstring ==="PM Light Snow / Wind") {return "snìhové pøeháòky vítr";}
+        else if( weatherstring ==="PM Rain") {return "déš";}
+        else if( weatherstring ==="PM Rain / Snow Showers") {return "déš, snìhové pøeháòky";}
+        else if( weatherstring ==="PM Rain / Snow") {return "déš sníh";}
+        else if( weatherstring ==="PM Rain / Wind") {return "déš vítr";}
+        else if( weatherstring ==="PM Showers") {return "pøeháòky";}
+        else if( weatherstring ==="PM Showers / Wind") {return "pøeháòky vítr";}
+        else if( weatherstring ==="PM Snow Showers") {return "snìhové pøeháòky";}
+        else if( weatherstring ==="PM Snow Showers / Wind") {return "snìhové pøeháòky vítr";}
+        else if( weatherstring ==="PM Snow") {return "sníh";}
+        else if( weatherstring ==="PM T-storms") {return "deštì";}
+        else if( weatherstring ==="PM Thundershowers") {return "polojasno";}
+        else if( weatherstring ==="PM Thunderstorms") {return "bouøka";}
+        else if( weatherstring ==="Rain and Snow") {return "déš a sníh";}
+        else if( weatherstring ==="Rain and Snow / Windy") {return "déš a sníh vìtrno";}
+        else if( weatherstring ==="Rain / Snow Showers / Wind") {return "déš, snìhové pøeháòky vítr";}
+        else if( weatherstring ==="Rain Early") {return "déš";}
+        else if( weatherstring ==="Rain Late") {return "déš";}
+        else if( weatherstring ==="Rain Shower") {return "sprchovı déš";}
+        else if( weatherstring ==="Rain Shower / Windy") {return "sprchovı déš vìtrno";}
+        else if( weatherstring ==="Rain to Snow") {return "déš se snìhem";}
+        else if( weatherstring ==="Rain") {return "déš";}
+        else if( weatherstring ==="Rain / Snow Early") {return "déš sníh";}
+        else if( weatherstring ==="Rain / Snow Late") {return "déš sníh";}
+        else if( weatherstring ==="Rain / Snow Showers Early") {return "déš se snìhem";}
+        else if( weatherstring ==="Rain / Snow Showers Late") {return "déš, snìhové pøeháòky";}
+        else if( weatherstring ==="Rain / Snow Showers") {return "déš, snìhové pøeháòky";}
+        else if( weatherstring ==="Rain / Snow") {return "déš sníh";}
+        else if( weatherstring ==="Rain / Snow / Wind") {return "déš sníh vítr";}
+        else if( weatherstring ==="Rain / Thunder") {return "déš hrom";}
+        else if( weatherstring ==="Rain / Wind Early") {return "déš vítr";}
+        else if( weatherstring ==="Rain / Wind Late") {return "déš vítr";}
+        else if( weatherstring ==="Rain / Wind") {return "déš vítr";}
+        else if( weatherstring ==="Rain / Windy") {return "déš vìtrno";}
+        else if( weatherstring ==="Scattered Showers") {return "polojasno";}
+        else if( weatherstring ==="Scattered Showers / Wind") {return "polojasno vítr";}
+        else if( weatherstring ==="Scattered Snow Showers") {return "rozptılené snìhové pøeháòky";}
+        else if( weatherstring ==="Scattered Snow Showers / Wind") {return "rozptılené snìhové pøeháòky vítr";}
+        else if( weatherstring ==="Scattered T-storms") {return "rozptılené deštì";}
+        else if( weatherstring ==="Scattered Thunderstorms") {return "bouøka";}
+        else if( weatherstring ==="Shallow Fog") {return "mìlká mlha";}
+        else if( weatherstring ==="Showers") {return "pøeháòky";}
+        else if( weatherstring ==="Showers Early") {return "pøeháòky";}
+        else if( weatherstring ==="Showers Late") {return "pøeháòky";}
+        else if( weatherstring ==="Showers in the Vicinity") {return "pøeháòky";}
+        else if( weatherstring ==="Showers / Wind") {return "pøeháòky vítr";}
+        else if( weatherstring ==="Sleet and Freezing Rain") {return "plískanice a mrznoucí déš";}
+        else if( weatherstring ==="Sleet / Windy") {return "plískanice vìtrno";}
+        else if( weatherstring ==="Snow Grains") {return "sníh zrna";}
+        else if( weatherstring ==="Snow Late") {return "sníh";}
+        else if( weatherstring ==="Snow Shower") {return "sníh pøeháòky";}
+        else if( weatherstring ==="Snow Showers Early") {return "snìhová pøeháòky";}
+        else if( weatherstring ==="Snow Showers Late") {return "snìhové pøeháòky";}
+        else if( weatherstring ==="Snow Showers") {return "snìhové pøeháòky";}
+        else if( weatherstring ==="Snow Showers / Wind") {return "snìhové pøeháòky vítr";}
+        else if( weatherstring ==="Snow to ;") {return "sníh";}
+        else if( weatherstring ==="Snow") {return "sníh";}
+        else if( weatherstring ==="Snow / Wind") {return "snow vítr";}
+        else if( weatherstring ==="Snow / Windy") {return "sníh vìtrno";}
+        else if( weatherstring ==="Squalls") {return "bouøí";}
+        else if( weatherstring ==="Sunny") {return "sluneèno";}
+        else if( weatherstring ==="Sunny / Wind") {return "sluneèno vítr";}
+        else if( weatherstring ==="Sunny / Windy") {return "sluneèno vìtrno";}
+        else if( weatherstring ==="T-showers") {return "pøeháòky";}
+        else if( weatherstring ==="Thunder in the Vicinity") {return "hrom";}
+        else if( weatherstring ==="Thunder") {return "hrom";}
+        else if( weatherstring ==="Thundershowers Early") {return "polojasno";}
+        else if( weatherstring ==="Thundershowers") {return "polojasno";}
+        else if( weatherstring ==="Thunderstorm") {return "bouøka";}
+        else if( weatherstring ==="Thunderstorm / Windy") {return "bouøka vìtrno";}
+        else if( weatherstring ==="Thunderstorms Early") {return "bouøka";}
+        else if( weatherstring ==="Thunderstorms Late") {return "bouøka";}
+        else if( weatherstring ==="Thunderstorms") {return "bouøka";}
+        else if( weatherstring ==="Unknown Precipitation") {return "neznámo";}
+        else if( weatherstring ==="Unknown") {return "neznámo";}
+        else if( weatherstring ==="Wintry Mix") {return "zimní mix";}
+        else {return weatherstring;}
+        }
